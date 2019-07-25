3d389350ef11b9931a48b1713f25f95f004a0bdf
Lucas Silva <emp.lucas.silva@gmail.com>
2019-07-25 16:26:15 -0300

    Added login token.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('login/', obtain_auth_token, name='login')
 ]
